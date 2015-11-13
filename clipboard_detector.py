58721675d56eda50caa0254c386e81eb40f54762
tmaeda1981jp <tmaeda1981jp@gmail.com>
2015-11-13 23:31:15 +0900

    Create class and rename file

---
+++ clipboard_detector.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- encoding:utf-8 -*-
+
+import time
+import sys
+import os
+import pyperclip
+
+
+class ClipboardDetector:
+
+
+    def __init__(self):
+        self.__memos = []
+
+
+    def watch(self):
+        while True:
+            text = pyperclip.paste()
+            self.append(text)
+            time.sleep(.1)
+
+
+    def append(self, text):
+        if text not in self.__memos:
+            self.__memos.append(text);
+            if 10 <= len(self.__memos):
+                self.__memos.pop(0)
+            print ''.join(' | '. join(self.__memos))
+
+
+clipboard = Clipboard()
+clipboard.watch()
